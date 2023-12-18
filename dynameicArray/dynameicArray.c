eb790b452133bff2fa49ae334cd0710cba3f43c0
挡泥板 <1573465644@qq.com>
2023-12-18 14:16:10 +0800

    [修改]完成动态数组初始化函数接口

--- dynameicArray/dynameicArray.c
+++ dynameicArray/dynameicArray.c
@@ -20,17 +20,21 @@ int dynameicArrayInit(dynameicArray *pArray,int capacity)
         return NULL_PTR;
     }
 
+    /* 避免传入非法值*/
     if(capacity < 0)
     {
         capacity = DEFAULT_SIZE;
     }
+    /* 分配空间*/
     pArray->data = (ELEMENTTYPE *)malloc(sizeof(ELEMENTTYPE) * capacity);
     if(pArray->data == NULL)
     {
         return MALLOC_ERROR;
     }
+    /* 清除脏数据*/
     memset(pArray->data, 0, sizeof(ELEMENTTYPE) * capacity);
 
+    /* 初始化动态数组的参数属性*/
     pArray->len = 0;
     pArray->capacity = capacity;
     return ON_SUCCESS;
