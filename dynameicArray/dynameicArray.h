f4dd670d17a7dda171c12581b339f935b2cd7833
挡泥板 <1573465644@qq.com>
2023-12-18 11:19:51 +0800

    [新增]动态数组的节点

--- dynameicArray/dynameicArray.h
+++ dynameicArray/dynameicArray.h
@@ -1,6 +1,14 @@
 #ifndef __DYNAMEIC_ARRAY_H_
 #define __DYNAMEIC_ARRAY_H_
-/* 避免头文件重复包含*/
 
+typedef int ELEMENTTYPE;
+/* 避免头文件重复包含*/
+typedef struct dynameicArray
+{
+    ELEMENTTYPE data;    /* 数组的空间*/
+    int len;     /* 数组的大小*/
+    int capacity;/* 数组的容量*/
+
+}dynameicArray;
 
 #endif
