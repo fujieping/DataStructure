8356ce3cbedc34fbc25eaeae69a8155476f8b570
挡泥板 <1573465644@qq.com>
2023-12-18 11:41:41 +0800

    [新增]动态数组的接口

--- dynameicArray/dynameicArray.h
+++ dynameicArray/dynameicArray.h
@@ -11,4 +11,32 @@ typedef struct dynameicArray
 
 }dynameicArray;
 
+/* API:application program interface.*/
+
+/* 动态数组的初始化*/
+int dynameicArrayInit();
+
+/* 动态数组插入数据(默认插到数组的末尾)*/
+int dynameicArrayInsertData();
+
+/* 动态数组插入数据，在指定位置插入*/
+int dynameicArrayAppointPosInserData();
+
+/* 动态数组修改指定位置数据*/
+int dynameicArrayModifyAppointPosData();
+
+/* 动态数组删除数据(默认删除数组末尾)*/
+int dynameicArrayDeleteData();
+
+/* 动态数组删除数据，删除指定数据*/
+int dynameicArrayDeleteAppointPosData();
+
+/* 动态数组销毁*/
+int dynameicArrayDestroy();
+
+/* 获取动态数组的大小*/
+int dynameicArrayGetSize();
+
+/* 获取动态数组的容量*/
+int dynameicArrayGetCapacity();
 #endif
