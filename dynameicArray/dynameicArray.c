209d9f04f725b3b451290eeb31d6f1063f4c9dc3
挡泥板 <1573465644@qq.com>
2023-12-18 15:18:11 +0800

    [修改]完成动态数组的插入的函数接口

--- dynameicArray/dynameicArray.c
+++ dynameicArray/dynameicArray.c
@@ -9,25 +9,30 @@ enum STATUS_CODE
     ON_SUCCESS,
     NULL_PTR,
     MALLOC_ERROR,
+    INVALID_ACCESS,
 };
 
 #define DEFAULT_SIZE 10
+
+/* 静态函数前置声明*/
+static int expandDynameicCapacity(dynameicArray *pArray);
+
 /* 动态数组的初始化*/
-int dynameicArrayInit(dynameicArray *pArray,int capacity)
+int dynameicArrayInit(dynameicArray *pArray, int capacity)
 {
-    if(pArray == NULL)
+    if (pArray == NULL)
     {
         return NULL_PTR;
     }
 
     /* 避免传入非法值*/
-    if(capacity < 0)
+    if (capacity < 0)
     {
         capacity = DEFAULT_SIZE;
     }
     /* 分配空间*/
     pArray->data = (ELEMENTTYPE *)malloc(sizeof(ELEMENTTYPE) * capacity);
-    if(pArray->data == NULL)
+    if (pArray->data == NULL)
     {
         return MALLOC_ERROR;
     }
@@ -41,10 +46,80 @@ int dynameicArrayInit(dynameicArray *pArray,int capacity)
 }
 
 /* 动态数组插入数据(默认插到数组的末尾)*/
-int dynameicArrayInsertData(dynameicArray *pArray, ELEMENTTYPE val);
+int dynameicArrayInsertData(dynameicArray *pArray, ELEMENTTYPE val)
+{
+    dynameicArrayAppointPosInserData(pArray, pArray->len, val);
+}
+
+/* 动态数组扩容*/
+static int expandDynameicCapacity(dynameicArray *pArray)
+{
+    int ret = 0;
+
+    int needexpandCapacity = pArray->capacity + (pArray->capacity >> 1);
+    /* 备份指针*/
+    ELEMENTTYPE * tmpPtr = pArray->data;
+    pArray->data = (ELEMENTTYPE *)malloc(sizeof(ELEMENTTYPE) * needexpandCapacity);
+    if (pArray->data == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+
+    /* 把之前的数据全部拷贝过来*/
+    for(int idx = 0; idx < pArray->len; idx++)
+    {
+        pArray->data[idx] = tmpPtr[idx];
+    }
+
+    /* 释放之前的内存*/
+    if(tmpPtr != NULL)
+    {
+        free(tmpPtr);
+        tmpPtr = NULL;
+    }
+
+    /* 更新动态数组容量*/
+    pArray->capacity = needexpandCapacity;
+
+    return ret;
+
+}
 
 /* 动态数组插入数据，在指定位置插入*/
-int dynameicArrayAppointPosInserData(dynameicArray *pArray, int pos, ELEMENTTYPE val);
+int dynameicArrayAppointPosInserData(dynameicArray *pArray, int pos, ELEMENTTYPE val)
+{
+    /* 指针判空*/
+    if (pArray == NULL)
+    {
+        return NULL_PTR;
+    }
+
+    /* 判断位置的合法性*/
+    if (pos < 0 || pos > pArray->len)
+    {
+        return INVALID_ACCESS;
+    }
+
+    /* 扩容:数组扩容的临界值是:数组的大小的1.5倍 >= 数组容量*/
+    if (pArray->len + (pArray->len >> 1) >= pArray->capacity)
+    {
+        /* 开始扩容*/
+        expandDynameicCapacity(pArray);
+    }
+
+    /* 数据后移，留出pos位置*/
+    for (int idx = pArray->len; idx > pos; idx--)
+    {
+        pArray->data[idx] = pArray->data[idx - 1];
+    }
+
+    /* 找到对应的位置填入值*/
+    pArray->data[pos] = val;
+
+    /* 数组大小加一*/
+    pArray->len++;
+    return ON_SUCCESS;
+}
 
 /* 动态数组修改指定位置数据*/
 int dynameicArrayModifyAppointPosData(dynameicArray *pArray, int pos, ELEMENTTYPE val);
